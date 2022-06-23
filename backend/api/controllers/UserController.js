8c743e75d9872e6077947d24de328be52105ff79
Eduardo Macias <maciastb@gmail.com>
2022-06-23 17:28:18 -0500

    Proyecto del curso de Sails de PLatzi

---
+++ backend/api/controllers/UserController.js
@@ -0,0 +1,65 @@
+const bcrypt = require('bcrypt');
+const joi = require('joi');
+const saltRound = 10;
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+
+  /**
+   * `UserController.signup()`
+   */
+  signup: async function (req, res) {
+    try {
+      const schema = joi.object().keys({
+        email: joi.string().required().email(),
+        password: joi.string().required()
+      });
+      const {email, password} = await schema.validateAsync(req.allParams());
+      const hashedPassword = await bcrypt.hash(password, saltRound);
+      const user = await User.create({email, password: hashedPassword}).fetch();
+      return res.ok(user);
+
+      // const params = await schema.validate(req.allParams());
+      // return res.ok(params);
+    } catch (err){
+      if(err.name === 'ValidationError'){
+        return res.badRequest({err}).json();
+      }
+      return res.serverError({err}).json();
+    }
+
+  },
+
+  /**
+   * `UserController.login()`
+   */
+  login: async function (req, res) {
+    try {
+      const schema = joi.object().keys({
+        email: joi.string().required().email(),
+        password: joi.string().required()
+      });
+      const {email, password} = await schema.validateAsync(req.allParams());
+      const user = await User.findOne({email});
+      if(!user){
+        return res.notFound({err: 'User not found'});
+      }
+      const comparedPassword = await bcrypt.compare(password, user.password);
+      const token = AuthenticationService.JWTIssuer({user: user.id}, '1 day');
+      return (comparedPassword) ? res.ok({token}) : res.badRequest({err: 'Unauthorized'});
+    } catch (err){
+      if(err.name === 'ValidationError'){
+        return res.badRequest({err}).json();
+      }
+      return res.serverError({err}).json();
+    }
+  }
+
+};
+
---
+++ backend/config/session.js
@@ -0,0 +1,39 @@
+/**
+ * Session Configuration
+ * (sails.config.session)
+ *
+ * Use the settings below to configure session integration in your app.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For all available options, see:
+ * https://sailsjs.com/config/session
+ */
+
+module.exports.session = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Session secret is automatically generated when your new app is created   *
+  * Replace at your own risk in production-- you will invalidate the cookies *
+  * of your users, forcing them to log in again.                             *
+  *                                                                          *
+  ***************************************************************************/
+  secret: 'db817c00f759e51f251de61ef4f6c322',
+
+
+  /***************************************************************************
+  *                                                                          *
+  * Customize when built-in session support will be skipped.                 *
+  *                                                                          *
+  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+  * session management when responding to simple requests for static assets, *
+  * like images or stylesheets.)                                             *
+  *                                                                          *
+  * https://sailsjs.com/config/session                                       *
+  *                                                                          *
+  ***************************************************************************/
+  // isSessionDisabled: function (req){
+  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+  // },
+
+};
